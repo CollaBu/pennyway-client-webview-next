8c38514eced6a2e104e56e4ac5856284ce1a27ac
이의찬 <cksxkr5193@naver.com>
2024-10-07 01:00:27 +0900

    ⚙️ 초기 프로젝트 설정 (#1)
    * feat: pnpm setting
    
    * feat: eslint version upgrade
    
    * feat: prettier setting
    
    * feat: zustand setting
    
    * feat: vitest setting
    
    * feat: react-query & msw setting
    
    * feat: ky setting
    
    * feat: PR Template setting
    
    * feat: fsd setting
    
    * feat: lint-staged & husky setting
    
    * fix: pre-commit
    
    * feat: eslint config file migration
    
    * fix: eslint reinstall
    
    * feat: metadata update
    
    * feat: pr template update

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Pennyway',
+  description: '💰 Pennyway : 지출 내역을 관리하고 공유하는 플랫폼',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
