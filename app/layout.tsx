78da9950328cf3fd128e0176d0f2c47ee314225b
이의찬 <cksxkr5193@naver.com>
2024-10-13 18:25:46 +0900

    ⚙️ 프로젝트 추가 설정 (#5)
    * feat: zustand version 4.5.5 재설치
    
    * feat: font 파일 추가
    
    * feat: logo.svg 추가
    
    * feat: network_error.png 추가
    
    * feat: 기존 file 삭제
    
    * feat: font 설정 완료
    
    * feat: eslint 추가 설정
    
    * feat: tanstack/react-query devtool setting
    
    * feat: QueryProvider setting
    
    * feat: font-family update
    
    * fix: eslint unused setting remove
    
    * feat: tailwind css createFontSize 함수 구현
    
    * fix: eslint.config.mjs remove
    
    eslint.config.mjs와 .eslintrc.json의 충돌로 인해 eslint가 정상적으로 작동하지 않아 삭제했습니다.
    
    * feat: eslint import update
    
    React와 Next import문은 반드시 맨 위로 가도록 pathGroupsExcludedImportTypes에 추가했습니다.
    사용하지 않는 styled component는 제거했습니다.
    
    * feat: delete fsd-import
    
    * feat: tailwind.config.ts update
    
    사용되지 않는 확장자 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+import { QueryProvider } from '@/app/provider';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Pennyway',
@@ -24,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,46 @@
 import type { Config } from 'tailwindcss';
 
+const createFontSize = (
+  size: string,
+  fontWeight: string,
+  lineHeight: string = '130%',
+): [string, { letterSpacing: string; lineHeight: string; fontWeight: string | number }] => [
+  size,
+  { letterSpacing: '-0.03em', lineHeight, fontWeight },
+];
+
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{ts,tsx}',
+    './src/pages/**/*.{ts,tsx}',
+    './src/features/**/*.{ts,tsx}',
+    './src/widgets/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      fontSize: {
+        h1b: createFontSize('24px', 'bold'),
+        h1s: createFontSize('24px', '600'),
+        h2s: createFontSize('20px', '600'),
+        h3b: createFontSize('16px', 'bold', '140%'),
+        h3s: createFontSize('16px', '600', '140%'),
+        h4s: createFontSize('14px', '600'),
+        h4m: createFontSize('14px', '500'),
+        b1s: createFontSize('12px', '600'),
+        b1m: createFontSize('12px', '500'),
+        b1r: createFontSize('12px', '400', '140%'),
+        b2s: createFontSize('11px', '600'),
+        b2m: createFontSize('11px', '500'),
+        b2r: createFontSize('11px', '400'),
+        b3m: createFontSize('9px', '500'),
+        b4m: createFontSize('8px', '500'),
       },
     },
   },
