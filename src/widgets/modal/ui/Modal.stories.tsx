a011da52907e45c17fc232818010102dab214bcf
BangDori <qudwns8865@naver.com>
2024-10-18 22:59:10 +0900

    feat: 모달창 스토리 추가

---
+++ src/widgets/modal/ui/Modal.stories.tsx
@@ -0,0 +1,30 @@
+import { fn } from '@storybook/test';
+
+import { Modal } from './Modal';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof Modal>;
+
+export default {
+  title: 'Modal',
+  component: Modal,
+  parameters: { layout: 'centered' },
+  tags: ['autodocs'],
+  args: {
+    onCancel: fn(),
+    onConfirm: fn(),
+  },
+} satisfies Meta<typeof Modal>;
+
+export const GoBackModal: Story = {
+  args: { type: 'goBack' },
+};
+
+export const DeletePostModal: Story = {
+  args: { type: 'deletePost' },
+};
+
+export const DeleteCommentModal: Story = {
+  args: { type: 'deleteComment' },
+};
