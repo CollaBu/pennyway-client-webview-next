762cc58224149b06d27e69bc3c3c972fcaa9aedc
BangDori <qudwns8865@naver.com>
2024-10-18 22:58:46 +0900

    feat: 모달창 위젯 추가

---
+++ src/widgets/modal/ui/Modal.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/shared/ui';
+
+import { modalContents } from '../consts';
+
+interface ModalProps {
+  type: keyof typeof modalContents;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+export function Modal({ type, onCancel, onConfirm }: ModalProps) {
+  return (
+    <div className="w-full h-full bg-white rounded-[10px]">
+      <div className="pt-7 pb-3 px-3.5 w-[230px]">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <h3 className="text-h3s text-black">{modalContents[type].title}</h3>
+          <p className="text-b1m text-gray04">{modalContents[type].description}</p>
+        </div>
+
+        <div className="flex justify-between mt-6 flex-1">
+          <Button type="default" label="취소" onClick={onCancel} />
+          <Button type="primary" label={modalContents[type].confirmText} onClick={onConfirm} />
+        </div>
+      </div>
+    </div>
+  );
+}
