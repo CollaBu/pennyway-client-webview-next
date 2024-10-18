d67219d948d321f5026a4cabf4e45f748568578f
BangDori <qudwns8865@naver.com>
2024-10-18 23:18:53 +0900

    feat: 모달 조작 훅스 추가

---
+++ src/widgets/modal/hooks/useModal.ts
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export function useModal() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const show = () => setIsVisible(true);
+  const hide = () => setIsVisible(false);
+
+  return { isVisible, show, hide };
+}
