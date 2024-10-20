96e12c507c49d82779f393198e1c0dd85bc8a55a
suhwan2004 <suhwan2004@gmail.com>
2024-10-21 00:25:55 +0900

    feat: storybook에서 svgr이 동작되도록 세팅

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -23,5 +23,28 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+      if (Array.isArray(test)) {
+        return test.some((t) => t instanceof RegExp && t.test('.svg'));
+      }
+
+      return test instanceof RegExp && test.test('.svg');
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 export default config;
