2559cfc81e4e58e3ac8ce793bd138d44b523ec6d
suhwan2004 <suhwan2004@gmail.com>
2024-10-21 00:22:12 +0900

    feat: svgr 설치 및 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        });
+        return config;
+      },
+      experimental: {
+        turbo: {
+          rules: {
+            "*.svg": {
+              loaders: ["@svgr/webpack"],
+              as: "*.js",
+            },
+          },
+        },
+      },
+};
 
 export default nextConfig;
