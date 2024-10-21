eaf48b73c9baede660dfd4bbfa64ed7d65087275
suhwan2004 <suhwan2004@gmail.com>
2024-10-21 22:10:11 +0900

    setting: next.config.mjs내 turbopack 대응 svg 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,6 @@ const nextConfig = {
         });
         return config;
       },
-      experimental: {
-        turbo: {
-          rules: {
-            "*.svg": {
-              loaders: ["@svgr/webpack"],
-              as: "*.js",
-            },
-          },
-        },
-      },
 };
 
 export default nextConfig;
