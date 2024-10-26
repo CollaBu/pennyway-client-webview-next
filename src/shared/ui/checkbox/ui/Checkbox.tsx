35071ca25348234b5149da650d95b4fb62932fde
suhwan2004 <suhwan2004@gmail.com>
2024-10-26 19:54:09 +0900

    feat: checkbox 공통 컴포넌트 구현

---
+++ src/shared/ui/checkbox/ui/Checkbox.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { checkboxShapes } from '../const';
+
+type setIsCheckedFunc = (value: boolean) => void;
+
+interface ICheckBox {
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>> | setIsCheckedFunc;
+  shape?: keyof typeof checkboxShapes;
+  text?: JSX.Element;
+}
+
+export const Checkbox = ({
+  isChecked,
+  setIsChecked,
+  shape = 'circleGraySmall',
+  text,
+}: ICheckBox) => {
+  const changeCheck = () => setIsChecked(!isChecked);
+
+  const CheckOn = checkboxShapes[shape].on;
+  const CheckOff = checkboxShapes[shape].off;
+
+  return (
+    <label className="cursor-pointer flex items-center">
+      <input
+        type="checkbox"
+        defaultChecked={isChecked}
+        onChange={changeCheck}
+        className="sr-only"
+      />
+      <span className="flex items-center justify-center">
+        {isChecked ? <CheckOn /> : <CheckOff />}
+      </span>
+      {text}
+    </label>
+  );
+};
