54efea79d242524af017edc9ec5e1b9782d5a738
suhwan2004 <suhwan2004@gmail.com>
2024-10-26 19:54:38 +0900

    feat: radio 공통 컴포넌트 구현

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,5 @@ export * from './dropdown';
 export * from './header';
 export * from './button';
 export * from './modal-wrapper';
+export * from './checkbox';
+export * from './radio';
---
+++ src/shared/ui/radio/ui/Radio.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { radioShapes } from '../const';
+
+interface IRadio {
+  isSelected: boolean;
+  changeSelected: () => void;
+  shape?: keyof typeof radioShapes;
+  text?: JSX.Element;
+  name: string; // 라디오 그룹을 위한 name 속성 추가
+}
+
+export const Radio = ({
+  isSelected,
+  changeSelected,
+  shape = 'circleGraySmall',
+  text,
+  name,
+}: IRadio) => {
+  const SelectOn = radioShapes[shape].on;
+  const SelectOff = radioShapes[shape].off;
+
+  return (
+    <label className="cursor-pointer flex items-center">
+      <input
+        type="radio"
+        checked={isSelected}
+        onChange={changeSelected}
+        className="sr-only"
+        name={name}
+      />
+      <span className="flex items-center justify-center">
+        {isSelected ? <SelectOn /> : <SelectOff />}
+      </span>
+      {text}
+    </label>
+  );
+};
