8e3096c619e983a1281e21abf06207eb9d36c4d9
suhwan2004 <suhwan2004@gmail.com>
2024-10-27 21:35:24 +0900

    chore: radio.stories.tsx 내 불필요 코드 제거

--- src/shared/ui/radio/ui/Radio.stories.tsx
+++ src/shared/ui/radio/ui/Radio.stories.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 
 import { Meta, StoryObj } from '@storybook/react';
 
-import { radioShapes } from '../consts';
-
 import { Radio, IRadio } from './Radio';
 
 type Story = StoryObj<typeof Radio>;
@@ -17,14 +15,6 @@ export default {
     shape: 'circleGraySmall',
     name: 'radioStory',
     text: <span>Option 1</span>,
-  },
-  argTypes: {
-    shape: {
-      control: {
-        type: 'select',
-        options: Object.keys(radioShapes),
-      },
-    },
   },
 } satisfies Meta<typeof Radio>;
 
@@ -50,37 +40,6 @@ export const Default: Story = {
           isSelected={selectedValue === 'option2'}
           changeSelected={() => handleChange('option2')}
           name="radioDefaultStory"
-          text={<span>Option 2</span>}
-        />
-      </div>
-    );
-  },
-};
-
-export const BoxPrimary: Story = {
-  render: (args: IRadio) => {
-    const [selectedValue, setSelectedValue] = useState<string>('');
-
-    const changeSelect = (value: string) => {
-      setSelectedValue(value);
-    };
-
-    return (
-      <div>
-        <Radio
-          {...args}
-          shape="boxPrimary"
-          isSelected={selectedValue === 'option1'}
-          changeSelected={() => changeSelect('option1')}
-          name="radioBoxPrimaryStory"
-          text={<span>Option 1</span>}
-        />
-        <Radio
-          {...args}
-          shape="boxPrimary"
-          isSelected={selectedValue === 'option2'}
-          changeSelected={() => changeSelect('option2')}
-          name="radioBoxPrimaryStory"
           text={<span>Option 2</span>}
         />
       </div>
