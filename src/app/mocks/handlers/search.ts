f4f2c06bca80b8ac711cf3c47a3a7d0fd6f2e242
BangDori <44726494+BangDori@users.noreply.github.com>
2024-11-01 00:28:58 +0900

    ✨ 사용자 검색 모킹 API Handler 추가 (#14)
    * feat: user 모킹 데이터 추가
    
    * feat: 사용자 검색 모킹 API 추가
    
    * style: 피드 검색 API Query 수정
    
    API Specification 문서에 나온대로 검색어에 대한 쿼리 변수명을 query -> target으로 변경
    
    * feat: Feed 모킹 데이터와 User 모킹 데이터 연결

--- src/app/mocks/handlers/search.ts
+++ src/app/mocks/handlers/search.ts
@@ -1,18 +1,19 @@
 import Fuse from 'fuse.js';
 import { http } from 'msw';
 
-import { feedMockData } from '../data';
+import { feedMockData, userMockData } from '../data';
 import { createHttpErrorResponse, createHttpSuccessResponse } from '../lib';
 
 export const searchHandlers = [
+  // 1️⃣ 피드 검색
   http.get('http://api.example.com/v2/feeds', ({ request }) => {
     const url = new URL(request.url);
 
-    const query = url.searchParams.get('query');
+    const target = url.searchParams.get('target');
     const page = Number(url.searchParams.get('page') || 1);
     const size = Number(url.searchParams.get('size') || 15);
 
-    if (!query || query.length < 2) {
+    if (!target || target.length < 2) {
       return createHttpErrorResponse('검색어는 최소 2글자 이상이어야 합니다');
     }
 
@@ -21,7 +22,7 @@ export const searchHandlers = [
     });
 
     const contents = fuse
-      .search(query)
+      .search(target)
       .map((result) => result.item)
       .slice((page - 1) * size, page * size);
     const totalFeeds = contents.length;
@@ -29,6 +30,40 @@ export const searchHandlers = [
 
     return createHttpSuccessResponse({
       feed: {
+        contents,
+        currentPageNumber: page,
+        pageSize: size,
+        numberOfElements: contents.length,
+        hasNextPage,
+      },
+    });
+  }),
+
+  // 2️⃣ 사용자 검색
+  http.get('http://api.example.com/v2/users', ({ request }) => {
+    const url = new URL(request.url);
+
+    const target = url.searchParams.get('target');
+    const page = Number(url.searchParams.get('page') || 1);
+    const size = Number(url.searchParams.get('size') || 15);
+
+    if (!target || target.length < 2) {
+      return createHttpErrorResponse('검색어는 최소 2글자 이상이어야 합니다');
+    }
+
+    const fuse = new Fuse(userMockData, {
+      keys: ['username'],
+    });
+
+    const contents = fuse
+      .search(target)
+      .map((result) => result.item)
+      .slice((page - 1) * size, page * size);
+    const totalFeeds = contents.length;
+    const hasNextPage = totalFeeds > page * size;
+
+    return createHttpSuccessResponse({
+      user: {
         contents,
         currentPageNumber: page,
         pageSize: size,
