7663f669b6540e5ec9683ed8325bfa0e47bbe80a
suhwan2004 <suhwan2004@gmail.com>
2024-11-04 21:46:05 +0900

    fix: feedHandlers - 신고하기 내 isBlind가 true일 시에 isBlinded가 true를 주도록 설정

--- src/app/mocks/handlers/feed.ts
+++ src/app/mocks/handlers/feed.ts
@@ -11,17 +11,19 @@ export const feedHandlers = [
   http.post('http://api.example.com/v2/feeds/:feedId/reports', async ({ request, params }) => {
     const { feedId } = params;
 
-    const { category, content } = (await request.json()) as IReportFeedReqDto;
+    const { category, content, isBlind } = (await request.json()) as IReportFeedReqDto;
 
     if (!feedId || !category || !content) {
       return createHttpErrorResponse('피드 ID, 신고 카테고리, 컨텐츠가 다 입력되어야 합니다.');
     }
 
-    feedMockData.forEach((cur) => {
-      if (cur.id === +feedId) {
-        cur.isBlinded = true;
-      }
-    });
+    if (isBlind) {
+      feedMockData.forEach((cur) => {
+        if (cur.id === +feedId) {
+          cur.isBlinded = true;
+        }
+      });
+    }
 
     return createHttpSuccessResponse({
       isReported: true,
