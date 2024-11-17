073edcd2546f0493e0d1c700f38e942228220ef7
suhwan2004 <suhwan2004@gmail.com>
2024-11-17 14:28:57 +0900

    feat: 피드 타입 정리

--- src/entities/feed/model/types.ts
+++ src/entities/feed/model/types.ts
@@ -1,13 +1,54 @@
+// 피드 신고
+export interface IReportFeedParamsDTO {
+  feedId: string;
+}
 export interface IReportFeedReqDTO {
   category: string;
   content: string;
   isBlind: boolean;
 }
 
-export interface IWriteFeedReqDTO {
+export interface IReportFeedResDTO {
+  isReported: boolean;
+}
+
+// 피드 작성
+export type TWriteFeedReqDTO = IFeedEditCommonDTO;
+
+// 피드 수정
+export type TModifyFeedReqDTO = IFeedEditCommonDTO;
+
+// common types...
+export type TFeedScope = 'public' | 'friends' | 'private';
+export interface IFeedEditCommonDTO {
   content: string;
   images: string[];
   scope: TFeedScope;
 }
+export interface IFeed {
+  id: number;
 
-export type TFeedScope = 'public' | 'friends' | 'private';
+  user: {
+    id: number;
+    profileImage: string;
+    username: string;
+  };
+
+  content: string;
+  images: IImage[];
+
+  likeCount: number;
+  commentCount: number;
+
+  isLiked: boolean;
+  isBookmarked: boolean;
+  isBlinded: boolean;
+
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IImage {
+  id: number;
+  imageUrl: string;
+}
