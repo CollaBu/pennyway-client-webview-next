5f714916ecd7f24922015c684c01750c1f8c52f7
Uichan Lee <uc.lee@tilda.co.kr>
2024-11-30 19:43:29 +0900

    feat: toast/consts 작성

---
+++ src/shared/ui/toast/consts/index.ts
@@ -0,0 +1,16 @@
+import IconCaution from '/public/assets/icon/icon-caution.svg';
+import IconFeedSiren from '/public/assets/icon/icon-feed-siren.svg';
+import IconCheckOnSmall from '/public/assets/icon/checkone-on-small.svg';
+
+export const toastIcon = {
+  caution: IconCaution,
+  feedSiren: IconFeedSiren,
+  checkOnSmall: IconCheckOnSmall,
+};
+
+export const toastMessage = {
+  reportSuccess: '신고가 접수되었어요',
+  reportFail: '다시 시도해 주세요',
+  networkError: '인터넷 연결이 불안정해요',
+  commonDeleteSuccess: '댓글이 삭제되었어요',
+};
