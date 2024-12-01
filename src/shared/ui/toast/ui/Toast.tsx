ac8a194fe5c6004dbb9b2994294a092c2d705800
Uichan Lee <uc.lee@tilda.co.kr>
2024-12-02 00:48:22 +0900

    feat: Toast.css 구현 및 적용

--- src/shared/ui/toast/ui/Toast.tsx
+++ src/shared/ui/toast/ui/Toast.tsx
@@ -2,6 +2,7 @@
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './Toast.css';
 
 export const Toast = () => {
   return (
@@ -10,33 +11,9 @@ export const Toast = () => {
       closeButton={false}
       hideProgressBar={true}
       closeOnClick={true}
-      autoClose={3000}
+      autoClose={300000}
       limit={1}
       theme="light"
-      style={{
-        position: 'fixed',
-        bottom: '8px',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        width: '87.5vw',
-        maxWidth: '280px',
-        margin: '0 auto',
-      }}
-      toastStyle={{
-        backgroundColor: '#98a3b4e5',
-        color: '#ffffff',
-        display: 'flex',
-        alignItems: 'center',
-        borderRadius: '6px',
-        width: '100%',
-        padding: '0 8px',
-      }}
-      bodyStyle={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: '6px',
-        height: '100%',
-      }}
       className="b1s"
     />
   );
